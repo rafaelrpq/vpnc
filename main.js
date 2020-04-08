fa4d755997c11feaca57e3517f85667be0a6b681
Rafael Quevedo <rafaelrpq@gmail.com>
2020-04-08 09:49:51 -0300

    adicionado icone e lançador do aplicativo

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//Icons made by https://www.flaticon.com/authors/smashicons
+
+/*
+    Este programa implementa uma interface para conexão vpn para acesso a rede da unipampa e seus TAEs/Profs
+
+    versão inicial ainda sem arquivo de configuração, ainda é necessário criar/editar o arquivo .conf que sera
+    lido pelo programa vpnc
+*/
+
 imports.gi.versions['Gtk'] = '3.0';
 const Gtk = imports.gi.Gtk;
 const GLib = imports.gi.GLib;
@@ -22,7 +31,7 @@ app.connect ('activate', () => {
     // … create a new window …
     let win = new Gtk.ApplicationWindow ({
         application    : app,
-        default_height : 250,
+        default_height : 150,
         default_width  : 600,
         resizable     : false
     });
@@ -48,47 +57,70 @@ app.connect ('activate', () => {
     let text = new Gtk.Label ();
     text.set_justify (Gtk.Justification.CENTER)
 
-    text.set_text (_text);
-    // … which closes the window when clicked
-    btn.connect ('clicked', () => {
-        if (vpnc()) {
-            let [success, output, error, status] = GLib.spawn_command_line_sync ( "pkexec vpnc-disconnect");
-            if (status == 0) {
-                btn.label = 'Conectar';
-                text.set_text ("A VPN não está conectada\n\nVocê não possui acesso a rede da UNIPAMPA");
-                ctx.remove_class ("destructive-action");
-                ctx.add_class ("suggested-action");
-                return ;
-            } else {
-               print (error);
-            }
-        } else {
-            let [success, output, error, status] = GLib.spawn_command_line_sync ( "pkexec vpnc myconf.conf");
-            if (status == 0) {
-                btn.label = 'Desconectar';
-                text.set_text ("A VPN está ativa\n\nVocê está conectado a UNIPAMPA");
-                ctx.remove_class ("suggested-action");
-                ctx.add_class ("destructive-action");
-                return ;
-            } else {
-                print (error);
-            }
-        }
-    });
+    text.set_markup ('<big>'+_text+'</big>');
+    // text.set_text (_text);
 
     let settings = Gtk.Settings.get_default();
 
     settings.gtk_application_prefer_dark_theme  = false;
 
     let img = new Gtk.Image()
-    img.set_from_icon_name ('dialog-warning',Gtk.IconSize.DIALOG);
-    // img.set_from_icon_name ('channel-secure-symbolic',Gtk.IconSize.DIALOG);
+    let icon = (vpnc ()) ? 'channel-secure-symbolic' : 'dialog-warning';
+    img.set_from_icon_name (icon, Gtk.IconSize.DIALOG);
 
     let grid = new Gtk.Grid ();
     grid.set_column_homogeneous (true);
-    grid.set_row_homogeneous (true);
-    grid.attach (img, 1, 1, 4, 1)
-    grid.attach (text, 2, 1, 10, 1)
+    grid.set_row_homogeneous (false);
+    grid.set_row_spacing (20);
+    grid.attach (new Gtk.Label(),  1, 1, 12, 1)
+    grid.attach (img,  1, 2, 4, 1)
+    grid.attach (text, 2, 2, 10, 1)
+    let msg = new Gtk.Label ();
+    grid.attach(msg, 1, 4, 12, 1);
+    // grid.attach (btn, 1, 2, 12, 1)
+
+    // … which closes the window when clicked
+    btn.connect ('clicked', () => {
+        if (vpnc()) {
+            let [success, output, error, status] = GLib.spawn_command_line_sync ( "pkexec vpnc-disconnect");
+            if (status == 0) {
+                btn.label = 'Conectar';
+                text.set_markup ("<big>A VPN não está conectada\n\nVocê não possui acesso a rede da UNIPAMPA</big>");
+                img.set_from_icon_name ('dialog-warning', Gtk.IconSize.DIALOG);
+                ctx.remove_class ("destructive-action");
+                ctx.add_class ("suggested-action");
+                return ;
+            } else {
+               // let info = new Gtk.InfoBar();
+               // let msg = new Gtk.Label (error);
+               // let content = info.get_content_area();
+               // content.container_add (msg);
+               // info.set_show_close_button (true);
+               // info.set_revealed (true);
+               // info.set_message_type (Gtk.MessageType.ERROR);
+               // info.connect ("response", () => {
+               //     Gtk.Widget.destroy();
+               // })
+               // grid.attach(info,0,0,1,1);
+               // info.show_all();
+               print (error)
+            }
+        } else {
+            msg.set_markup ('<i>Conectando...</i>');
+            let [success, output, error, status] = GLib.spawn_command_line_sync ( "pkexec vpnc myconf.conf");
+            if (status == 0) {
+                btn.label = 'Desconectar';
+                text.set_markup ("<big>A VPN está ativa\n\nVocê está conectado a UNIPAMPA</big>");
+                img.set_from_icon_name ('channel-secure-symbolic', Gtk.IconSize.DIALOG);
+                ctx.remove_class ("suggested-action");
+                ctx.add_class ("destructive-action");
+                msg.set_text ('');
+                return ;
+            } else {
+                msg.set_markup ('<i>'+error.toString().trim()+'</i>');
+            }
+        }
+    });
 
     win.add (grid);
     header.pack_end(btn);
